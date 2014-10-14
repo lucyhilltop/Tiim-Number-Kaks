9dab236a503c314e3609f24dc8a8082ffd1fc9fe
Lucy Hilltop <liisupzz@gmail.com>
2014-10-14 15:32:51 +0300

    Intro

--- POS/src/ee/ut/math/tvt/tiim_number_kaks/Intro.java
+++ POS/src/ee/ut/math/tvt/tiim_number_kaks/Intro.java
@@ -1,8 +1,16 @@
 package ee.ut.math.tvt.tiim_number_kaks;
 
-public class Intro {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+public class Intro extends Application {
+	final static IntroUI ui = new IntroUI();
+	@Override
+	public void start(Stage primaryStage) {
+		ui.start(primaryStage);
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
