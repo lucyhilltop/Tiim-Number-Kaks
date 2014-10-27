ec5ce0b8b68c45a4ad6f142cbf6df652a2dea9da
silverwithp <silverpajumae@hotmail.com>
2014-10-27 19:03:12 +0200

    Intro uuesti tehtud, jookseb koos uue lisatud asjadega (Intro lisad)

--- POS/src/ee/ut/math/tvt/tiim_number_kaks/Intro.java
+++ POS/src/ee/ut/math/tvt/tiim_number_kaks/Intro.java
@@ -1,20 +1,47 @@
 package ee.ut.math.tvt.tiim_number_kaks;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Intro extends Application {
+import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
+
+public class Intro {
+	
+	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	final static IntroUI ui = new IntroUI();
 	 
-	public void start(Stage primaryStage) throws IOException {
-		ui.start(primaryStage);
-	}
 
 	public static void main(String[] args) {
-		launch(args);
+
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
+		
 	}
 }
--- POS/src/ee/ut/math/tvt/tiim_number_kaks/IntroUI.java
+++ POS/src/ee/ut/math/tvt/tiim_number_kaks/IntroUI.java
@@ -1,114 +1,61 @@
 package ee.ut.math.tvt.tiim_number_kaks;
 
-import java.io.FileNotFoundException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Properties;
-import java.util.Scanner;
+class IntroUI extends JFrame {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+JPanel p1;
+Dimension d;
 
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+    public IntroUI() {
+        createAndShowGUI();
+    }
+    
+    private void createAndShowGUI()
+    {
+        setTitle("Intro");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        
+        // An empty panel with FlowLayout
+        p1=new JPanel();
+        
+        
+        // Set some preferred size
+        d=new Dimension(300,200);
+        p1.setPreferredSize(d);
+        
+    	p1.add(new JLabel("Tiim Number 2"));
+    	p1.add(new JLabel("Silver Pajumäe"));
+    	p1.add(new JLabel("silverpajumae@hotmail.com"));
+    	p1.add(new JLabel("Silver Pajumäe, Liis Mäeots, Mikk Tark"));
+    	
+		BufferedImage wPic = null;
+		try {
+			wPic = ImageIO.read(this.getClass().getResource("number.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	p1.add(new JLabel(new ImageIcon(wPic)));
+        
+        // Add panels
+        add(p1);
+        
+        setSize(400,400);
+        setVisible(true);
+        pack();
 
-public class IntroUI extends Application {
-	
-	public String getAppProps() throws IOException {
-		String result = "";
-		Properties prop = new Properties();
-		
-		InputStream inputStream = getClass().getResourceAsStream("application.properties");
-		prop.load(inputStream);
-		
-		if (inputStream == null) {
-			throw new FileNotFoundException("property file 'applications.properties' not found in the classpath");
-		}
-		
-		String nimi = prop.getProperty("Tiim");
-		String juht = prop.getProperty("Juht");
-		String juhimeil = prop.getProperty("email");
-		String liikmed = prop.getProperty("Liikmed");
-		result = "Tiimi nimi: " + nimi + "\n" + "Tiimi juht: " + juht + "\n" + 
-				"Juhi e-mail: " + juhimeil + "\n" + "Liikmed: " + liikmed;
-		return result;
-	}
-	
-	public String getVersionProps() throws IOException {
-		String result = "";
-		Properties prop = new Properties();
-		String propFileName = "version.properties";
-		
-		InputStream inputStream = getClass().getResourceAsStream("version.properties");
-		prop.load(inputStream);
-		if (inputStream == null) {
-			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-		}
-		
-		String major = prop.getProperty("build.major.number");
-		String minor = prop.getProperty("build.minor.number");
-		String build = prop.getProperty("build.number");
-		result = major + "." + minor + "." + build;
-		return result;
-	}
-	
-	public void start(Stage primaryStage) throws IOException {
-		primaryStage.setTitle("Tiim number 2");
-		Group juur = new Group();
-		Text Tiim= new Text();
-		Tiim.setX(10);
-		Tiim.setY(20);
-		Tiim.setCache(true);
-		IntroUI a_props = new IntroUI();
-        Tiim.setText(a_props.getAppProps());
-        
-        Image logo = new Image(getClass().getResourceAsStream("number.png"));
-        ImageView iv1 = new ImageView();
-        iv1.setImage(logo);
-		
-		/*Text Juht= new Text();
-		Juht.setX(10);
-		Juht.setY(40);
-		Juht.setCache(true);
-        Juht.setText(list1.get(3));
-        
-		Text Juhi_email= new Text();
-		Juhi_email.setX(10);
-		Juhi_email.setY(60);
-		Juhi_email.setCache(true);
-		Juhi_email.setText(list1.get(5));
-		
-		Text Liikmed= new Text();
-		Liikmed.setX(10);
-		Liikmed.setY(80);
-		Liikmed.setCache(true);
-		Liikmed.setText(list1.get(7));*/
-		
-		Text Software_version= new Text();
-		Software_version.setX(10);
-		Software_version.setY(100);
-		Software_version.setCache(true);
-		Software_version.setText(a_props.getVersionProps());
-//		Software_version.setText(list2.get(list1.size()-3) + '.' + list1.get(list1.size()-1) + '.' + list1.get(lis));
-		
-		
-		juur.getChildren().add(Tiim);
-		/*juur.getChildren().add(Juht);
-		juur.getChildren().add(Juhi_email);
-		juur.getChildren().add(Liikmed);*/
-		juur.getChildren().add(Software_version);
-		
-		
-		primaryStage.setScene(new Scene(juur));
-		HBox box = new HBox();
-		box.setLayoutX(260);
-        box.getChildren().add(iv1);
-        juur.getChildren().add(box);        
-	    primaryStage.show();
-		
-	}
+    }
+    
+    public static void main(String args[])
+    {
+        new IntroUI();
+    }
 }
