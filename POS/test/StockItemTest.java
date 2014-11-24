c921b1999ed21f9d08b73cf6362fd5d743e30f15
Lucy Hilltop <liisupzz@gmail.com>
2014-11-24 17:41:54 +0200

    Esimene klass on Junit testidega tehtud

--- POS/test/StockItemTest.java
+++ POS/test/StockItemTest.java
@@ -1,4 +1,33 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
 
 public class StockItemTest {
-
+	private StockItem item1;
+	//private StockItem item2;
+	private Long id1;
+	//private Long id2;
+	
+	@Before
+	public void SetUp() {
+		id1 = new Long(777);
+		//id2 = new Long(778);
+		item1 = new StockItem(id1, "Test-stock1", "teststock1", 11.0, 3);
+		//item2 = new StockItem(id2, "Test-stock2", "teststock2", 12.0, 3);
+	}
+	
+	@Test
+	public void testClone(){
+		assertSame(item1,item1.clone());
+	}
+	
+	@Test
+	public void testGetColumn(){
+		
+	}
 }
--- POS/test/StockTableModelTest.java
+++ POS/test/StockTableModelTest.java
@@ -1,4 +1,15 @@
 
 public class StockTableModelTest {
-
+	public void testValidateNameUniqueness(){
+		
+	}
+	public void testHasEnoughInStock(){
+		
+	}
+	public void testGetItemByIdWhenItemExists(){
+		
+	}
+	public void testGetItemByIdWhenThrowsException(){
+		
+	}
 }
