e15beb8f3288ddafe535e204a0d6ecaa8bd36221
Lucy Hilltop <liisupzz@gmail.com>
2014-11-24 18:27:57 +0200

    stocktablemodeltest pooleli

--- POS/test/StockItemTest.java
+++ POS/test/StockItemTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
@@ -9,30 +9,26 @@ import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class StockItemTest {
 	private StockItem item1;
-	//private StockItem item2;
 	private Long id1;
 	private String nimi;
 	private String desc;
 	private double hind;
 	private int kogus;
-	//private Long id2;
 	
 	@Before
 	public void SetUp() {
 		id1 = new Long(777);
-		//id2 = new Long(778);
 		nimi = "Test-stock1";
 		desc = "teststock1";
 		hind = 11.0;
 		kogus = 3;
 		item1 = new StockItem(id1, nimi, desc, hind, kogus);
-		//item2 = new StockItem(id2, "Test-stock2", "teststock2", 12.0, 3);
 	}
 	
-	/*@Test
+	@Ignore
 	public void testClone(){
 		assertEquals(item1,item1.clone());
-	}*/
+	}
 	
 	@Test
 	public void testGetColumn(){
--- POS/test/StockTableModelTest.java
+++ POS/test/StockTableModelTest.java
@@ -1,14 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
+
 
 public class StockTableModelTest {
+	private StockItem item1;
+	private Long id1;
+	private String nimi;
+	private double hind;
+	private int kogus;
+	private StockItem item2;
+	private Long id2;
+	private StockTableModel tabel;
+	
+	@Before
+	public void SetUp(){
+		
+	}
+	
+	@Test
 	public void testValidateNameUniqueness(){
 		
 	}
+	
+	@Test
 	public void testHasEnoughInStock(){
 		
 	}
+	
+	@Test
 	public void testGetItemByIdWhenItemExists(){
 		
 	}
+	
+	@Test
 	public void testGetItemByIdWhenThrowsException(){
 		
 	}
